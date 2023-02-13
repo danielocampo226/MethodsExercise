923fdab7dd8a4f932e921a9d875c22c8f9b306c1
danielocampo226 <sierradanny562@gmail.com>
2023-02-13 18:37:54 -0500

    finished profile set

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,25 +4,34 @@
     {
         static void Main(string[] args)
         {
-             //Name: Michael
+            //Name: Michael
 
-             //Favorite Color: Blue
+            //Favorite Color: Blue
 
-             //Favorite Animal: Walrus
+            //Favorite Animal: Walrus
 
             // Favorite Band: The Beatles
 
-            Console.WriteLine("Helloo - What is your first name?");
+            Console.WriteLine("Hello - What is your first name?");
             var userName = Console.ReadLine();
 
             Console.WriteLine($"Hi, {userName}. What is your favorite color?");
             var color = Console.ReadLine();
-            
+
             Console.WriteLine($"{color} is an aweseome color! What is your favorite animal?");
             var animal = Console.ReadLine();
 
             Console.WriteLine("Great! Now, what is your favorite band?");
             var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+            
 
         }
     }
