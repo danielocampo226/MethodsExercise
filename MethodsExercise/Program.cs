6b93760fbaf69afb56502190a464e9204a10d2ba
danielocampo226 <sierradanny562@gmail.com>
2023-02-13 18:32:34 -0500

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             //Name: Michael
+
+             //Favorite Color: Blue
+
+             //Favorite Animal: Walrus
+
+            // Favorite Band: The Beatles
+
+            Console.WriteLine("Helloo - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+            
+            Console.WriteLine($"{color} is an aweseome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+
         }
     }
 }
