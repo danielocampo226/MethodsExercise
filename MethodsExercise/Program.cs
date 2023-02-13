89fe0c3607fca1411ed69b6ab99de7bd198466d4
danielocampo226 <sierradanny562@gmail.com>
2023-02-13 18:52:18 -0500

    MethodsExercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,8 +2,23 @@
 {
     public class Program
     {
+
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        } 
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
         static void Main(string[] args)
         {
+            var amountOfCars = Sum(2, 6);
+            var amountOfAnts = Multiply(60, 2, 4);
+
+            //--------------------------------------------------------------
             //Name: Michael
 
             //Favorite Color: Blue
